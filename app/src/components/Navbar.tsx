e0e8d928656a1498e3675c9fe22c0b7e134137f2
mbenz12 <mbenz10120112@gmail.com>
2023-03-23 08:32:48 -0400

    wallet button added

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { motion } from "framer-motion"
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Link from 'next/link';
 import Image from 'next/image';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { getSliceAddress } from '@/utils';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 
-const NavbarAnchor = ({active}: {active: boolean}) => {
+const NavbarAnchor = ({ active }: { active: boolean }) => {
   return (
     <motion.div
       className="absolute -bottom-2 left-0 h-1 w-full bg-gradient-underline"
@@ -41,14 +42,15 @@ export const Navbar: React.FC = () => {
             <p className="opacity-50 text-[13px]">BALANCE</p>
             <p className="text-[17px]">75O ZEN</p>
           </div>
-          <Image width={46} height={46} src="/images/coin.png" alt=""/>
+          <Image width={46} height={46} src="/images/coin.png" alt="" />
         </div>
         <div className="flex justify-end gap-3">
           <div className="flex flex-col justify-center text-right">
             {wallet.publicKey && <p className="opacity-50 text-[13px]">{getSliceAddress(wallet.publicKey.toString())}</p>}
             <p className="text-[17px]">Pablo</p>
           </div>
-          <Image width={46} height={46} src="/images/avatar.png" alt=""/>
+          <Image width={46} height={46} src="/images/avatar.png" alt="" />
+          <WalletMultiButton />
         </div>
       </div>
     </>
