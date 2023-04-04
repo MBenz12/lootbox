a6c2dcbb85f04add1950b63ea59f9bb0ee6ac74a
mbenz12 <mbenz10120112@gmail.com>
2023-04-04 16:34:38 -0400

    db connection changed for event listener

--- app/src/db/connect.ts
+++ app/src/db/connect.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+const MONGODB_URL = 'mongodb+srv://doadmin:T59j637L28DC4mQf@db-mongodb-sfo3-35637-7da3f833.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-sfo3-35637';
+
 // @ts-ignore
 let cached = global.mongoose;
 
@@ -20,7 +22,7 @@ async function connect() {
       bufferCommands: true,
     };
 
-    cached.promise = mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '', opts).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
