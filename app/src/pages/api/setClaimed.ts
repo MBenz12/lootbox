df2a6e80b22acd40f4d8f67c3c3f9fd9dcdbda39
mbenz12 <mbenz10120112@gmail.com>
2023-04-04 16:16:25 -0400

    set claimed updated in admin panel

--- app/src/pages/api/setClaimed.ts
+++ app/src/pages/api/setClaimed.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Claim from '@/db/models/Claim';
+import { ObjectId } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import connect from '@/db/connect';
 
@@ -8,11 +9,7 @@ export default async function handler(
   res: NextApiResponse<string>
 ) {
   await connect();
-  const { claimIndex } = req.body;
-  const claims = await Claim.find();
-  const claim = claims[claimIndex];
-  if (claim) {
-    await Claim.deleteOne(claim);
-  }
+  const { claimId } = req.body;
+  await Claim.deleteOne({ _id: new ObjectId(claimId) });
   res.status(200).json("success");
 }
