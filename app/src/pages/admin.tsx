334e451b33eb814806650840d9de950f26a6a3be
mbenz12 <mbenz10120112@gmail.com>
2023-04-04 22:13:02 -0400

    admin panel authorization added

--- app/src/pages/admin.tsx
+++ app/src/pages/admin.tsx
@@ -3,14 +3,18 @@ import Head from 'next/head';
 import Sidebar from '@/sections/admin/Sidebar';
 import Main from '@/sections/admin/Main';
 import useFetchAllLootboxes from '@/hooks/useFetchAllLootboxes';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { getRole } from '@/utils';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export default function Admin() {
+  const { publicKey } = useWallet();
   const [reload, setReload] = useState({});
   const { lootboxes } = useFetchAllLootboxes(reload);
   const [name, setName] = useState('Lootbox 1');
-  const boxes = useMemo(() => { 
+  const boxes = useMemo(() => {
     const names = lootboxes.map(lootbox => lootbox.name);
-    if (names.length) { 
+    if (names.length) {
       setName(names[0]);
     }
     return names;
@@ -20,7 +24,7 @@ export default function Admin() {
     const boxName = `Lootbox ${boxes.length + 1}`;
     setName(boxName);
   }
-  
+
   return (
     <>
       <Head>
@@ -29,10 +33,15 @@ export default function Admin() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={"flex admin-page py-16 px-5"}>
-        <Sidebar boxes={boxes} currentBox={name} setCurrentBox={setName} createNewBox={createNewBox} />
-        <Main name={name} setName={setName} setReload={setReload} reload={reload} />
-      </div>
+      {publicKey && getRole(publicKey) > 0 ?
+        <div className={"flex admin-page py-16 px-5"}>
+          <Sidebar boxes={boxes} currentBox={name} setCurrentBox={setName} createNewBox={createNewBox} />
+          <Main name={name} setName={setName} setReload={setReload} reload={reload} />
+        </div> :
+        <div className='flex items-center justify-center w-full min-h-[80vh]'>
+          <p>Connect authorized wallet</p>
+        </div>
+      }
     </>
   );
 };
