3a646d4b27daa119afb8948554eb4d8071880add
mbenz12 <mbenz10120112@gmail.com>
2023-04-14 13:19:47 -0400

    added padding on recently won prizes swiper

--- app/src/sections/lootboxes/RecentlyWonPrizes.tsx
+++ app/src/sections/lootboxes/RecentlyWonPrizes.tsx
@@ -9,7 +9,7 @@ export const RecentlyWonPrizes = ({ events }: { events: Array<Event> }) => {
   return (
     <div className="my-5">
       <h1 className={"font-akira font-[800] uppercase text-xl my-5"} style={{ transform: "scaleY(75%)" }}>Recently Won Prizes</h1>
-      <Swiper freeMode={true} spaceBetween={15} slidesPerView="auto" modules={[FreeMode]} className={"prizes-wrapper p-4"}>
+      <Swiper freeMode={true} spaceBetween={15} slidesPerView="auto" modules={[FreeMode]} className={"prizes-wrapper"} style={{ padding: '16px' }}>
         {
           events.map((event, index) => (
             <SwiperSlide key={index}>
