9e4cfbcbabbd073b0974727fff56db23094d3d50
mbenz12 <mbenz10120112@gmail.com>
2023-04-26 08:37:31 -0400

    fixed sidebar for admin panel

--- app/src/sections/admin/Sidebar.tsx
+++ app/src/sections/admin/Sidebar.tsx
@@ -13,16 +13,16 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ boxes: boxIds, currentBox, setCurrentBox, createNewBox }) => {
   const { reload } = useContext(ReloadContext);
   const { boxes } = useFetchBoxes(reload);
-  const name = useMemo(() => {
-    let box = getBox(boxes, currentBox);
-    return box ? box.name : 'Free';
-  }, [boxes, currentBox]);
   return (
     <div className={"h-full gap-2 flex flex-col min-w-[150px]"}>
       {
-        boxIds.map((id, index) => (
-          <div key={index} onClick={() => setCurrentBox(id)} className={"cursor-pointer w-fit " + (id === currentBox ? "opacity-100" : "opacity-50")}>{name}</div>
-        ))
+        boxIds.map((id, index) => {
+          let box = getBox(boxes, id);
+          const name = box ? box.name : 'Free';
+          return (
+            <div key={index} onClick={() => setCurrentBox(id)} className={"cursor-pointer w-fit " + (id === currentBox ? "opacity-100" : "opacity-50")}>{name}</div>
+          )
+        })
       }
       <div onClick={() => createNewBox()} className={"mt-16 cursor-pointer opacity-50 w-fit"}>+ NEW BOX</div>
     </div>
