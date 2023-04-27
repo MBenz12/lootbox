e151cbc37e30495c202499b7dcff35bdb862fe35
mbenz12 <mbenz10120112@gmail.com>
2023-04-27 17:03:09 -0400

    box name show for play events

--- app/src/pages/api/getPlayEvents.ts
+++ app/src/pages/api/getPlayEvents.ts
@@ -2,6 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import PlayEvent from '@/db/models/PlayEvent';
 import connect from '@/db/connect';
+import Box from '@/db/models/Box';
+
+const getBox = (boxes: Array<any>, id: string) => {
+  let index = boxes.map(box => box.id).indexOf(id);
+  if (index !== -1) {
+    return boxes[index];
+  }
+  return null;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,6 +18,13 @@ export default async function handler(
 ) {
   await connect();
   const events = await PlayEvent.find();
+  const boxes = await Box.find();
   events.reverse();
+  events.forEach(event => {
+    let box = getBox(boxes, event.lootboxName);
+    if (box) {
+      event.lootboxName = box.name;
+    }
+  })
   res.status(200).json(events.slice(0, 10));
 }
