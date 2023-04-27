a034ea808f280231b1fef140dd357c76ed4bf3fa
mbenz12 <mbenz10120112@gmail.com>
2023-04-27 10:45:13 -0400

    claim page responsive added

--- app/src/sections/claim/Heading.tsx
+++ app/src/sections/claim/Heading.tsx
@@ -2,9 +2,9 @@ import {Button} from "@/components/lootboxes/Button";
 
 const Heading = ({ handleClaimAll }: { handleClaimAll: () => void }) => {
   return (
-    <div className={"relative w-fit py-6 flex gap-4 place-items-center claim-heading"}>
-      <h1 className={"font-akira font-[800] text-[38px]"}>CLAIM PRIZES</h1>
-      <Button text={"Claim All Prizes"} handler={handleClaimAll} />
+    <div className={"relative w-fit py-6 flex gap-4 place-items-center claim-heading flex-wrap"}>
+      <h1 className={"font-akira font-[800] sm:text-[38px] text-[24px]"}>CLAIM PRIZES</h1>
+      <Button text={"Claim All"} handler={handleClaimAll} />
     </div>
   );
 };
