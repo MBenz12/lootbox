e5a7debf05fbfa0457217808097a2d7eed1cfb21
mbenz12 <mbenz10120112@gmail.com>
2023-04-29 08:53:27 -0400

    decimal changed

--- app/src/components/admin/NftCard.tsx
+++ app/src/components/admin/NftCard.tsx
@@ -17,7 +17,7 @@ const NftCard: React.FC<NftCardProps> = ({ image, price, symbol, handleDelete })
       </div>
       {
         !image && (
-          <p className={"flex select-none justify-center place-items-center h-full font-bold bg-white text-black rounded-t-xl"}>{price !== undefined ? price.toLocaleString('en-us', { maximumFractionDigits: 2 }): ''} {symbol}</p>
+          <p className={"flex select-none justify-center place-items-center h-full font-bold bg-white text-black rounded-t-xl"}>{price !== undefined ? price.toLocaleString('en-us', { maximumFractionDigits: 3 }): ''} {symbol}</p>
         )
       }
       <p className={"text-center text-[12px] py-1 bg-white text-black rounded-b-xl"}>{price !== undefined ? `${price.toLocaleString('en-us', { maximumFractionDigits: 3 })} SOL` : '-'}</p>
