b8a8f4af21d9b9d558a8bb98b74849fc7c14977b
mbenz12 <mbenz10120112@gmail.com>
2023-05-04 09:48:36 -0400

    backdrop added in home page

--- app/src/sections/lootboxes/Banner.tsx
+++ app/src/sections/lootboxes/Banner.tsx
@@ -8,10 +8,12 @@ const slides = [
 
 export const Banner: React.FC = () => {
   return (
-    <div className="relative banner">
+    <div className="relative banner overflow-hidden">
       <div className='absolute left-0 top-0 w-full h-full -z-10'>
         <Carousel slides={slides} interval={5000} />
       </div>
+      <div className='absolute left-0 -top-[50%] -translate-x-[50%] w-[400px] h-[200%] bg-[#0b0c1a] blur-[50px] -z-[5] sm:block hidden'></div>
+      <div className='absolute right-0 -top-[50%] translate-x-[50%] w-[400px] h-[200%] bg-[#0b0c1a] blur-[50px] -z-[5] sm:block hidden'></div>
       <p className="font-extrabold uppercase text-[62px] font-akira leading-tight pt-[20px] lg:text-[110px] lg:pt-[40px]">UKIYO</p>
       <p className="text-[22px] font-america-mono mt-[-13px] lg:text-[36px] lg:mt-[-20px]"><span className="text-[#FF6C6C] font-america-mono">LOOT</span>BOX</p>
     </div>
